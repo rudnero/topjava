7590b27ec0cd2976f0cde09d91ff3bb49e168978
vrudnov <rudnero@gmail.com>
2019-06-07 19:18:29 +0300

    1 0 fix

---
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class MealTo {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean excess;
+
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+}
