475cdf226588b90c8b6aa64b366b9299059c1eec
vrudnov <rudnero@gmail.com>
2019-08-13 15:27:03 +0300

    HW09 Base + Optional

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,25 +1,37 @@
+const mealAjaxUrl = "ajax/profile/meals/";
+
 function updateFilteredTable() {
     $.ajax({
         type: "GET",
-        url: "ajax/profile/meals/filter",
+        url: mealAjaxUrl + "filter",
         data: $("#filter").serialize()
     }).done(updateTableByData);
 }
 
 function clearFilter() {
     $("#filter")[0].reset();
-    $.get("ajax/profile/meals/", updateTableByData);
+    $.get(mealAjaxUrl, updateTableByData);
 }
 
 $(function () {
     makeEditable({
-        ajaxUrl: "ajax/profile/meals/",
+        ajaxUrl: mealAjaxUrl,
         datatableApi: $("#datatable").DataTable({
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
+                    "data": "dateTime",
+                    "render": function(data, type, row) {
+                        if (type === "display") {
+                            return data.replace("T", " ");
+                        }
+                        return data;
+                    }
                 },
                 {
                     "data": "description"
@@ -28,12 +40,14 @@ $(function () {
                     "data": "calories"
                 },
                 {
-                    "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderEditBtn
                 },
                 {
-                    "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -41,8 +55,55 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                if (!data.enabled) {
+                    $(row).attr("data-mealExcess", data.excess);
+                }
+            }
         }),
         updateTable: updateFilteredTable
+    });
+
+    jQuery('#startDate').datetimepicker({
+        format:'Y-m-d',
+        onShow:function( ct ){
+            this.setOptions({
+                maxDate:jQuery('#endDate').val()?jQuery('#endDate').val():false
+            })
+        },
+        timepicker:false
+    });
+    jQuery('#endDate').datetimepicker({
+        format:'Y-m-d',
+        onShow:function( ct ){
+            this.setOptions({
+                minDate:jQuery('#startDate').val()?jQuery('#startDate').val():false
+            })
+        },
+        timepicker:false
+    });
+
+    jQuery('#startTime').datetimepicker({
+        format:'H:i',
+        onShow:function( ct ){
+            this.setOptions({
+                maxTime:jQuery('#endTime').val()?jQuery('#endTime').val():false
+            })
+        },
+        datepicker:false
+    });
+    jQuery('#endTime').datetimepicker({
+        format:'H:i',
+        onShow:function( ct ){
+            this.setOptions({
+                minTime:jQuery('#startTime').val()?jQuery('#startTime').val():false
+            })
+        },
+        datepicker:false
+    });
+
+    $('#dateTime').datetimepicker({
+        format: 'Y-m-d H:i'
     });
 });
